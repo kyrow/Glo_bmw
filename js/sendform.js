8392b4c3f03b56d8bceb383800477cd61eebedcb
Ampersander <mr.khan4ik@gmail.com>
2021-05-21 21:37:36 +0600

    sendForm

---
+++ js/sendform.js
@@ -0,0 +1,54 @@
+const server = 'https://jsonplaceholder.typicode.com/posts'
+
+const sendData = (data, callBack, falseCallBck) => {
+	const request = new XMLHttpRequest();
+	request.open('POST', server);
+
+	request.addEventListener('readystatechange', () => {
+		if (request.readyState !== 4) return;
+		if (request.status === 200 || request.status === 201) {
+			const response = JSON.parse(request.responseText);
+			callBack(response.id);
+		} else {
+			falseCallBck(request.statusText)
+			throw new Error(request.status)
+		}
+	});
+
+	request.send(data)
+
+};
+
+const formElems = document.querySelectorAll('.form');
+
+const formHandler = (form) => {
+	form.addEventListener('submit', (event) => {
+		event.preventDefault();
+		const data = {};
+
+		for (const { name, value } of form.elements) {
+			if (name) {
+				data[name] = value
+
+			}
+		}
+
+		const smallElem = document.createElement('small');
+
+		sendData(JSON.stringify(data),
+			(id) => {
+				smallElem.innerHTML = 'Ваша заявка ' + id + '!\n В ближайшее время свяжемся';
+				smallElem.style.color = 'green';
+				form.append(smallElem);
+			},
+			(err) => {
+				smallElem.textContent = 'Error:' + err;
+				smallElem.style.color = 'red';
+				form.append(smallElem);
+			});
+		form.reset();
+	})
+};
+
+formElems.forEach(formHandler)
+
